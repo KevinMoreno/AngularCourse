d6bb3ee51aef7add7fad609342dbe5106316fea5
KevinMoreno <kvn_mrn@live.com.mx>
2016-09-14 12:57:42 -0600

    ng source, tabs

--- www/js/app.js
+++ www/js/app.js
@@ -1,50 +1,62 @@
 (function(){
-	
+
 	var gem = {
 		name: 'Azurite',
 		price: '2.95',
-		description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+		description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam ipsum, iusto voluptates laudantium quam et repellendus facilis tenetur quasi ea, doloremque veritatis velit in, laboriosam quibusdam vero incidunt ipsam voluptatum.',
 		canPurchase: false,
 		soldOut: true
 	};
 
 	var gems = [
 		{
-		name: 'Azurite',
-		price: '2.95',
-		description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-		canPurchase: true,
-		soldOut: true,
-		images: [
-			{id:0, url:'img/gem-01.gif'},
-			{id:1, url:'img/gem-02.gif'},
-			{id:2, url:'img/gem-03.gif'}
+			name: 'Azurite',
+			price: '2.95',
+			description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam ipsum, iusto voluptates laudantium quam et repellendus facilis tenetur quasi ea, doloremque veritatis velit in, laboriosam quibusdam vero incidunt ipsam voluptatum.',
+			canPurchase: false,
+			soldOut: true,
+			images: [
+				{id:0, url:'img/gem-01.gif'},
+				{id:1, url:'img/gem-02.gif'},
+				{id:2, url:'img/gem-03.gif'}
+			],
+			reviews: [
+				{stars: 5, body: 'I love this product', author:'alonso@gmail.com'},
+				{stars: 5, body: 'I love this product', author:'alonso@gmail.com'}
 			]
 		},
 		{
-		name: 'Bloodstone',
-		price: '5.95',
-		description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-		canPurchase: true,
-		soldOut: true,
-		images: [
-			{id:0, url:'img/gem-04.gif'},
-			{id:1, url:'img/gem-05.gif'},
-			{id:2, url:'img/gem-06.gif'}
+			name: 'Bloodstone',
+			price: '5.95',
+			description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam ipsum, iusto voluptates laudantium quam et repellendus facilis tenetur quasi ea, doloremque veritatis velit in, laboriosam quibusdam vero incidunt ipsam voluptatum.',
+			canPurchase: false,
+			soldOut: true,
+			images: [
+				{id:0, url:'img/gem-04.gif'},
+				{id:1, url:'img/gem-05.gif'},
+				{id:2, url:'img/gem-06.gif'}
+			],
+			reviews: [
+				{stars: 5, body: 'I love this product', author:'alonso@gmail.com'},
+				{stars: 5, body: 'I love this product', author:'alonso@gmail.com'}
 			]
 		},
 		{
-		name: 'Zircon',
-		price: '3.95',
-		description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-		canPurchase: true,
-		soldOut: true,
-		images: [
-			{id:0, url:'img/gem-07.gif'},
-			{id:1, url:'img/gem-08.gif'},
-			{id:2, url:'img/gem-09.gif'}
-		]
-		},	
+			name: 'Zircon',
+			price: '3.95',
+			description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam ipsum, iusto voluptates laudantium quam et repellendus facilis tenetur quasi ea, doloremque veritatis velit in, laboriosam quibusdam vero incidunt ipsam voluptatum.',
+			canPurchase: false,
+			soldOut: true,
+			images: [
+				{id:0, url:'img/gem-07.gif'},
+				{id:1, url:'img/gem-08.gif'},
+				{id:2, url:'img/gem-09.gif'}
+			],
+			reviews: [
+				{stars: 5, body: 'I love this product', author:'alonso@gmail.com'},
+				{stars: 5, body: 'I love this product', author:'alonso@gmail.com'}
+			]
+		}	
 	];
 
 	var app = angular.module('gemStore', []);
@@ -53,5 +65,75 @@
 		this.products = gems;
 	});
 
-})();
+	app.controller('PanelController', function(){
+		this.tab = 1;
 
+		this.selectTab = function(setTab){
+			this.tab = setTab;
+		};
+
+		this.isSelected = function(checkTab){
+			return this.tab === checkTab;
+		};
+	});
+
+	app.controller('ReviewsController',function(){
+      this.review = {};
+      this.addReview = function(product){
+      	product.reviews.push(this.review);
+      	this.review = {};
+      }
+
+	});
+
+	/*app.directive('productTitle', function(){
+		return{
+			restrict: 'E',
+			templateUrl: 'product-title.html'
+		};
+	});
+*/
+	app.directive('productTitle', function(){
+		return{
+			restrict: 'A',
+			templateUrl: 'product-title.html'
+		};
+	});
+
+	app.directive('productPanels', function(){
+		return{
+			restrict: 'E',
+			templateUrl: 'product-panels.html',
+			controller: function(){
+				this.tab = 1;
+
+				this.selectTab = function(setTab){
+					this.tab = setTab;
+				};
+
+				this.isSelected = function(checkTab){
+					return this.tab === checkTab;
+				};
+			},
+			controllerAs: 'panel'
+		}
+	});
+
+	app.directive('reviewsDirective', function(){
+		return{
+			restrict: 'E',
+			templateUrl: 'reviews.html',
+			controller: function(){
+				this.review = {};
+			    this.addReview = function(product){
+			      	product.reviews.push(this.review);
+			      	this.review = {};
+			    }
+			},
+			controllerAs: 'reviewCtrl'
+		}
+	})
+
+
+
+})();
